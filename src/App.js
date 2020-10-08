a5ed6395458544dbf9b0c5c2fbc532809b86f219
Sophie Kaelin <sophie.m.kaelin@gmail.com>
2020-10-08 16:42:48 +1100

    standard carousel working (no styling)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import logo from "./logo.svg"
+import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import PosterCarousel from "./components/PosterCarousel.js"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    return (
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            <PosterCarousel />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    )
 }
 
-export default App;
+export default App
