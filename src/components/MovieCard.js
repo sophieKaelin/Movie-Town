ccdbde68c2a0da2b1c034266103728a8675bf5c8
George Tsamoglou <george.gt39@gmail.com>
2020-10-18 00:29:20 +1100

    updates to movie card ui

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,26 +1,35 @@
 import React from "react"
 import "bootstrap/dist/css/bootstrap.css"
 import { Card, Row, Col } from "react-bootstrap"
-import tmnt from "../moviePosters/tmnt.png"
 
 const MovieCard = () => {
 	return (
-		<Card style={{ width: "62rem" }}>
-			<Row>
-				<Col>
+		<Card className="mt-5" style={{ width: "56rem", margin: "auto auto" }}>
+			<Row className="no-gutters">
+				<Col className="col-4">
 					<Card.Img
 						className="col-auto img-fluid"
 						src="//placehold.it/200"
 					/>
 				</Col>
 				<Col>
-					<Card.Title>Movie Title</Card.Title>
-					<Card.Text>
-						Some quick example text to build on the card title and
-						make up the bulk of the card's content.
-					</Card.Text>
+					<Card.Body>
+						<Card.Title>
+							<h3>Movie Title</h3>
+						</Card.Title>
+						<Card.Text>
+							RATING | RUNTIME | GENRE | RELEASE DATE
+						</Card.Text>
+						<Card.Text>
+							Some quick example text to build on the card title
+							and make up the bulk of the card's content.
+						</Card.Text>
+					</Card.Body>
 				</Col>
 			</Row>
+			<Card.Footer className="text-muted">
+				Movie added 18/10/20
+			</Card.Footer>
 		</Card>
 	)
 }
