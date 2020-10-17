e56f4e046f0aafa9f409fed26076fd12227d2bce
Sophie Kaelin <sophie.m.kaelin@gmail.com>
2020-10-17 13:20:31 +1100

    add path to server

--- server/server.js
+++ server/server.js
@@ -1,16 +1,22 @@
-const http = require('http')
-const express = require('express') 
-const cors = require('cors')
-const fs = require('fs') 
-const apiRouter = require('./api')
+require("dotenv").config()
+const http = require("http")
+const express = require("express")
+const cors = require("cors")
+const fs = require("fs")
+const apiRouter = require("./api")
 
-const app = express() 
+const app = express()
 app.use(cors())
-app.use(express.json()) 
-app.use(express.static('build'))
+app.use(express.json())
+app.use(express.static("build"))
 app.use(apiRouter)
+
+const path = require("path")
+app.get("*", (request, response) => {
+	response.sendFile(path.join(__dirname, "../build/index.html"))
+})
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+	console.log(`Server running on port ${PORT}`)
 })
