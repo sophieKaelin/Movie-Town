1f168edc1431e9a7e1bf09bc7f5442355598fd40
bradleykenny <bkenny283@gmail.com>
2020-10-17 22:25:36 +1100

    Added SCSS for custom Bootstrap and made theme PURPLE.

--- src/App.js
+++ src/App.js
@@ -1,65 +1,59 @@
-import React, { useState, useEffect } from "react";
-import "./style/App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import axios from 'axios';
-import PosterCarousel from "./components/PosterCarousel.js";
-import Login from "./components/Login.js";
-import Register from "./components/Register.js";
-import NavBar from "./components/NavBar.js";
-import Profile from "./components/Profile.js";
+import React, { useState, useEffect } from "react"
+import "./style/App.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import axios from "axios"
+import PosterCarousel from "./components/PosterCarousel.js"
+import Login from "./components/Login.js"
+import Register from "./components/Register.js"
+import NavBar from "./components/NavBar.js"
+import Profile from "./components/Profile.js"
 import {
 	BrowserRouter as Router,
 	Switch,
 	Route,
 	Redirect,
-} from "react-router-dom";
+} from "react-router-dom"
 
 function App() {
+	const [users, setUsers] = useState([]) //all users
+	const [reviews, setReviews] = useState([]) //all reviews
 
-    const [users, setUsers] = useState([]) //all users
-    const [reviews, setReviews] = useState([]) //all reviews
+	const baseURL = "/api/"
 
-    const baseURL = "/api/"
+	const addNewReview = (newReview) => {
+		axios.post(baseURL + "reviews", newReview).then((response) => {
+			console.log(response)
+			setReviews([...reviews, response.data])
+		})
+	}
 
-    const addNewReview = (newReview) => {
-        axios.post(baseURL + "reviews", newReview)
-          .then(response => {
-            console.log(response)
-            setReviews([...reviews, response.data])
-          })
-    }
+	const deleteReview = (review) => {
+		console.log("delete", review)
+		axios.delete(baseURL + "reviews/" + review.id).then((response) => {
+			console.log("delete succeeded")
+			const newReviews = reviews.filter((r) => r.id !== review.id)
+			setReviews(newReviews)
+		})
+	}
 
-    const deleteReview = (review) => {
-        console.log("delete", review)
-        axios.delete(baseURL + "reviews/" + review.id)
-            .then((response) => {
-            console.log("delete succeeded")
-            const newReviews = reviews.filter(r => r.id !== review.id)
-            setReviews(newReviews)
-        })
-    }
+	const addNewUser = (newUser) => {
+		axios.post(baseURL + "users", newUser).then((response) => {
+			console.log(response)
+			setUsers([...users, response.data])
+		})
+	}
 
-    const addNewUser = (newUser) => {
-        axios.post(baseURL + "users", newUser)
-            .then(response => {
-            console.log(response)
-            setUsers([...users, response.data])
-        })
-    }
+	useEffect(() => {
+		axios.get(baseURL + "users").then((response) => {
+			setUsers(response.data)
+		})
+	}, [])
 
-    useEffect(() => {
-        axios.get(baseURL + "users")
-        .then((response) => {
-            setUsers(response.data)
-        })
-    }, [])
-
-    useEffect(() => {
-        axios.get(baseURL + "reviews")
-        .then((response) => {
-            setReviews(response.data)
-        })
-    }, [])
+	useEffect(() => {
+		axios.get(baseURL + "reviews").then((response) => {
+			setReviews(response.data)
+		})
+	}, [])
 
 	return (
 		<Router>
@@ -84,7 +78,7 @@ function App() {
 				</Route>
 			</Switch>
 		</Router>
-	);
+	)
 }
 
-export default App;
+export default App
