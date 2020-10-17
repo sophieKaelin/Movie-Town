9161d5d4394d1f48c1c6c92778f47be811f0cef4
bradleykenny <bkenny283@gmail.com>
2020-10-17 14:13:29 +1100

    Changes to connect to OMDB API.

--- server/imdbApi.js
+++ server/imdbApi.js
@@ -1,6 +1,13 @@
 const express = require("express")
 const fs = require("fs")
+const axios = require("axios")
 
 const IMDBRouter = express.Router()
+
+const ombd = process.env.OMBD_API
+
+IMDBRouter.get("/api/movie/title", (req, res) => {
+	return JSON.parse(axios.get(omdb + "t=" + req.body.title))
+})
 
 module.exports = IMDBRouter
