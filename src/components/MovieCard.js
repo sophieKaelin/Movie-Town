0b712b30500c313756d81a2ddc150c68ad386d29
George Tsamoglou <george.gt39@gmail.com>
2020-10-18 15:27:54 +1100

    more updates to movie card ui

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,29 +1,37 @@
 import React from "react"
 import "bootstrap/dist/css/bootstrap.css"
 import { Card, Row, Col } from "react-bootstrap"
+import avengersEndgame from "../moviePosters/avengersEndgame.jpg"
 
 const MovieCard = () => {
+	const title = "Avengers Endgame"
+	const year = "2019"
+	const rating = "M"
+	const runtime = "3h 1min"
+	const genre = "Action, Adventure, Drama"
+	const releaseDate = "24 April 2019 (Australia)"
+	const synopsis =
+		"After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe."
 	return (
 		<Card className="mt-5" style={{ width: "56rem", margin: "auto auto" }}>
 			<Row className="no-gutters">
 				<Col className="col-4">
 					<Card.Img
 						className="col-auto img-fluid"
-						src="//placehold.it/200"
+						src={avengersEndgame}
 					/>
 				</Col>
 				<Col>
 					<Card.Body>
 						<Card.Title>
-							<h3>Movie Title</h3>
+							<h3>
+								{title}, {year}
+							</h3>
 						</Card.Title>
 						<Card.Text>
-							RATING | RUNTIME | GENRE | RELEASE DATE
+							{rating} | {runtime} | {genre} | {releaseDate}
 						</Card.Text>
-						<Card.Text>
-							Some quick example text to build on the card title
-							and make up the bulk of the card's content.
-						</Card.Text>
+						<Card.Text>{synopsis}</Card.Text>
 					</Card.Body>
 				</Col>
 			</Row>
