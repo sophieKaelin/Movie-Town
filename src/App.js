a94cd88edb566e2c0c0684d85d07b9765a36d809
Sophie Kaelin <sophie.m.kaelin@gmail.com>
2020-10-19 21:13:21 +1100

    Save current user (Maintain session w localstore

--- src/App.js
+++ src/App.js
@@ -1,39 +1,102 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./style/App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import PosterCarousel from "./components/PosterCarousel.js";
-import Login from "./components/Login.js";
-import Register from "./components/Register.js";
-import NavBar from "./components/NavBar.js";
+import React, { useState, useEffect } from "react"
+import "./style/App.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import axios from "axios"
+import PosterCarousel from "./components/PosterCarousel.js"
+import Login from "./components/Login.js"
+import Register from "./components/Register.js"
+import NavBar from "./components/NavBar.js"
+import Profile from "./components/Profile.js"
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from "react-router-dom";
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect,
+} from "react-router-dom"
+
+//TODO: change this to relative path when pushed to heroku
+const userURL = "http://localhost:3001/api/users/"
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/home">
-          <NavBar />
-          <PosterCarousel />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/register">
-          <Register />
-        </Route>
-        {/* ALWAYS LEAVE LAST */}
-        <Route path="/">
-          <Redirect to="/home" />
-        </Route>
-      </Switch>
-    </Router>
-  );
+    const [users, setUsers] = useState([]) //all users
+    const [user, setUser] = useState(null) //Logged In User
+    const [reviews, setReviews] = useState([]) //all reviews
+
+    const FsetUser = (user) => {
+        setUser(user)
+    }
+
+    //TODO: Remove this because we shouldn't be using local storage
+    useEffect(() => {
+        const localUser = JSON.parse(localStorage.getItem("user"))
+        if (localUser) {
+            axios.get(userURL + localUser.username).then((response) => {
+                setUser(response.data)
+            })
+        }
+    }, [])
+
+    const baseURL = "/api/"
+
+    const addNewReview = (newReview) => {
+        axios.post(baseURL + "reviews", newReview).then((response) => {
+            console.log(response)
+            setReviews([...reviews, response.data])
+        })
+    }
+
+    const deleteReview = (review) => {
+        console.log("delete", review)
+        axios.delete(baseURL + "reviews/" + review.id).then((response) => {
+            console.log("delete succeeded")
+            const newReviews = reviews.filter((r) => r.id !== review.id)
+            setReviews(newReviews)
+        })
+    }
+
+    const addNewUser = (newUser) => {
+        axios.post(baseURL + "users", newUser).then((response) => {
+            console.log(response)
+            setUsers([...users, response.data])
+        })
+    }
+
+    useEffect(() => {
+        axios.get(baseURL + "users").then((response) => {
+            setUsers(response.data)
+        })
+    }, [])
+
+    useEffect(() => {
+        axios.get(baseURL + "reviews").then((response) => {
+            setReviews(response.data)
+        })
+    }, [])
+
+    return (
+        <Router>
+            <Switch>
+                <Route path="/home">
+                    <NavBar />
+                    <PosterCarousel />
+                </Route>
+                <Route path="/login">
+                    <Login user={user} setUser={FsetUser} />
+                </Route>
+                <Route path="/register">
+                    <Register />
+                </Route>
+                <Route path="/profile">
+                    <NavBar />
+                    <Profile />
+                </Route>
+                {/* ALWAYS LEAVE LAST */}
+                <Route path="/">
+                    <Redirect to="/home" />
+                </Route>
+            </Switch>
+        </Router>
+    )
 }
 
-export default App;
+export default App
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,131 +1,169 @@
-import React, { useState } from "react";
-import "bootstrap/dist/css/bootstrap.css";
-import { Button, Form, Container, Row, Col, Card } from "react-bootstrap";
-import "../style/Login.css";
+import React, { useState } from "react"
+import axios from "axios"
+import "bootstrap/dist/css/bootstrap.css"
+import { Button, Form, Container, Row, Col, Card } from "react-bootstrap"
+import "../style/Login.css"
+import { Redirect } from "react-router-dom"
+
+//TODO: change this to relative path when pushed to heroku
+const loginURL = "http://localhost:3001/api/login"
 
 const validateForm = (errors) => {
-  let valid = true;
-  Object.values(errors).forEach(
-    // if we have an error string set valid to false
-    (val) => val.length > 0 && (valid = false)
-  );
-  return valid;
-};
+    let valid = true
+    Object.values(errors).forEach(
+        // if we have an error string set valid to false
+        (val) => val.length > 0 && (valid = false)
+    )
+    return valid
+}
 
-const Login = () => {
-  const [loginInfo, setLoginInfo] = useState({ username: "", password: "" });
-  const [errors, setErrors] = useState({ username: "", password: "" });
+const Login = ({ user, setUser }) => {
+    const [loginInfo, setLoginInfo] = useState({ username: "", password: "" })
+    const [errors, setErrors] = useState({ username: "", password: "" })
 
-  const handleChange = (e) => {
-    e.preventDefault();
-    const { name, value } = e.target;
-
-    switch (name) {
-      case "username":
-        if (value.length < 4) {
-          setErrors({
-            ...errors,
-            username: "Username must be at least 4 characters long!",
-          });
-          setLoginInfo({ ...loginInfo, username: "" });
-        } else {
-          setErrors({
-            ...errors,
-            username: "",
-          });
-          setLoginInfo({ ...loginInfo, username: value });
-        }
-        break;
-
-      case "password":
-        if (value.length < 2) {
-          setErrors({
-            ...errors,
-            password: "Password must be at least 2 characters long!",
-          });
-          setLoginInfo({ ...loginInfo, password: "" });
-        } else {
-          setErrors({
-            ...errors,
-            password: "",
-          });
-          setLoginInfo({ ...loginInfo, password: value });
-        }
-        break;
-
-      default:
-        break;
-    }
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (validateForm(errors)) {
-      console.info("Valid Form");
-    } else {
-      console.error("Invalid Form");
+    const login = () => {
+        return axios.post(loginURL, loginInfo).then((response) => response.data)
     }
 
-    // Logging
-    console.log("Submit username: ", loginInfo.username);
-    console.log("Submit password: ", loginInfo.password);
-  };
-  return (
-    <div id="loginBackground">
-      <Container>
-        <Row className="d-flex justify-content-center">
-          <Col sm={4} className="align-items-center">
-            <Card bg="light" className="loginCard">
-              <Card.Body>
-                <Card.Title className="text-center">
-                  <h1>Login</h1>
-                </Card.Title>
-                <Form onSubmit={handleSubmit}>
-                  <Form.Group controlId="formUsername">
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control
-                      name="username"
-                      type="username"
-                      placeholder="Username"
-                      onChange={handleChange}
-                    />
-                    {errors.username.length > 0 && (
-                      <Form.Text className="error">{errors.username}</Form.Text>
-                    )}
-                  </Form.Group>
+    const handleChange = (e) => {
+        e.preventDefault()
+        const { name, value } = e.target
 
-                  <Form.Group controlId="formPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control
-                      name="password"
-                      type="password"
-                      placeholder="Password"
-                      onChange={handleChange}
-                    />
-                    {errors.password.length > 0 && (
-                      <Form.Text className="error">{errors.password}</Form.Text>
-                    )}
-                  </Form.Group>
+        switch (name) {
+            case "username":
+                if (value.length < 4) {
+                    setErrors({
+                        ...errors,
+                        username:
+                            "Username must be at least 4 characters long!",
+                    })
+                    setLoginInfo({ ...loginInfo, username: "" })
+                } else {
+                    setErrors({
+                        ...errors,
+                        username: "",
+                    })
+                    setLoginInfo({ ...loginInfo, username: value })
+                }
+                break
 
-                  <Button variant="primary" type="submit" className="btn-block">
-                    Submit
-                  </Button>
-                </Form>
-              </Card.Body>
-              <Card.Footer>
-                <p className="font-small grey-text d-flex justify-content-end">
-                  Don't have an account?
-                  <a href="/register" className="blue-text ml-1">
-                    Sign Up
-                  </a>
-                </p>
-              </Card.Footer>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
-};
+            case "password":
+                if (value.length < 2) {
+                    setErrors({
+                        ...errors,
+                        password:
+                            "Password must be at least 2 characters long!",
+                    })
+                    setLoginInfo({ ...loginInfo, password: "" })
+                } else {
+                    setErrors({
+                        ...errors,
+                        password: "",
+                    })
+                    setLoginInfo({ ...loginInfo, password: value })
+                }
+                break
 
-export default Login;
+            default:
+                break
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (validateForm(errors)) {
+            console.info("Valid Form")
+            login()
+                .then((data) => {
+                    console.log("Success:", data)
+                    console.log("uuuu", data.username)
+                    setUser(data.username)
+                    console.log("user is: ", user)
+                    //TODO: Don't set things in local storage, that's naughty. We should hash the login.
+                    localStorage.setItem("user", JSON.stringify(data))
+                })
+                .catch((error) => {
+                    console.log("Error:", error)
+                })
+        } else {
+            console.error("Invalid Form")
+        }
+
+        // Logging
+        console.log("Submit username: ", loginInfo.username)
+        console.log("Submit password: ", loginInfo.password)
+    }
+
+    if (user) {
+        return <Redirect to="/" />
+    } else
+        return (
+            <div id="loginBackground">
+                <Container>
+                    <Row className="d-flex justify-content-center">
+                        <Col sm={4} className="align-items-center">
+                            <Card bg="light" className="loginCard">
+                                <Card.Body>
+                                    <Card.Title className="text-center">
+                                        <h1>Login</h1>
+                                    </Card.Title>
+                                    <Form onSubmit={handleSubmit}>
+                                        <Form.Group controlId="formUsername">
+                                            <Form.Label>Username</Form.Label>
+                                            <Form.Control
+                                                name="username"
+                                                type="username"
+                                                placeholder="Username"
+                                                onChange={handleChange}
+                                            />
+                                            {errors.username.length > 0 && (
+                                                <Form.Text className="error">
+                                                    {errors.username}
+                                                </Form.Text>
+                                            )}
+                                        </Form.Group>
+
+                                        <Form.Group controlId="formPassword">
+                                            <Form.Label>Password</Form.Label>
+                                            <Form.Control
+                                                name="password"
+                                                type="password"
+                                                placeholder="Password"
+                                                onChange={handleChange}
+                                            />
+                                            {errors.password.length > 0 && (
+                                                <Form.Text className="error">
+                                                    {errors.password}
+                                                </Form.Text>
+                                            )}
+                                        </Form.Group>
+
+                                        <Button
+                                            variant="primary"
+                                            type="submit"
+                                            className="btn-block"
+                                        >
+                                            Submit
+                                        </Button>
+                                    </Form>
+                                </Card.Body>
+                                <Card.Footer>
+                                    <p className="font-small grey-text d-flex justify-content-end">
+                                        Don't have an account?
+                                        <a
+                                            href="/register"
+                                            className="blue-text ml-1"
+                                        >
+                                            Sign Up
+                                        </a>
+                                    </p>
+                                </Card.Footer>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        )
+}
+
+export default Login
