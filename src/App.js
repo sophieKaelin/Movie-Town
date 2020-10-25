e4f1f715cccd3015b515730fc3cc41cca1f21fd8
Sophie Kaelin <sophiekaelin@Sophies-MacBook-Pro.local>
2020-10-25 23:20:34 +1100

    dynamic data on profile page (dodgy)

--- src/App.js
+++ src/App.js
@@ -25,94 +25,105 @@ const userURL = "http://localhost:3001/api/users/"
 
 function App() {
 	const [users, setUsers] = useState([]) //all users
-	const [user, setUser] = useState(null) //Logged In User
+	const [user, setUser] = useState("") //Logged In User
 	const [reviews, setReviews] = useState([]) //all reviews
 
 	const FsetUser = (user) => {
 		setUser(user)
-    }
+	}
 
 	//TODO: Remove this because we shouldn't be using local storage
-	useEffect(() => {
+	useEffect(async () => {
 		const localUser = JSON.parse(localStorage.getItem("user"))
 		if (localUser) {
-			axios.get(userURL + localUser.username).then((response) => {
+			await axios.get(userURL + localUser.username).then((response) => {
 				setUser(response.data)
 			})
 		}
 	}, [])
-    
-    const addNewUser = (newUser) => {
-        userServices.addNewUser(newUser)
-        .then((response) => {
-            console.log(response)
-            setUsers([...users, response.data])
-        })}
-    const followUser = (userToFollow, user) => {
-        userServices.followUser
-        .then(response => {
-            setUser(response.data)
-            setUsers(users.map(u => u.id !== user.id ? u : response.data))
-        })}
-    const unfollowUser = (userToFollow, user) => {
-        userServices.unfollowUser
-        .then(response => {
-            setUser(response.data)
-            setUsers(users.map(u => u.id !== user.id ? u : response.data))
-        })}
-    const addWatched = (titleid, user) => {
-        userServices.addWatched
-        .then(response => {
-            setUser(response.data)
-            setUsers(users.map(u => u.id !== user.id ? u : response.data))
-        })}
-    const addToWatch = (titleid, user) => {
-        userServices.addToWatch
-        .then(response => {
-            setUser(response.data)
-            setUsers(users.map(u => u.id !== user.id ? u : response.data))
-        })}
 
-    const addNewReview = (newReview) => {
-        reviewServices.addNewReview
-        .then((response) => {
-            console.log(response)
-            setReviews([...reviews, response.data])
-        })}
-    const deleteReview = (review) => {
-        reviewServices.deleteReview
-        .then((response) => {
-            console.log("delete succeeded")
-            const newReviews = reviews.filter((r) => r.id !== review.id)
-            setReviews(newReviews)
-        })}
-    const likeReview = (review, user) => {
-        reviewServices.likeReview
-        .then(response => {
-            setReviews(reviews.map(r => r.id !== review.id ? r : response.data))
-        })}
-    const unlikeReview = (review, user) => {
-        reviewServices.unlikeReview
-        .then(response => {
-            setReviews(reviews.map(r => r.id !== review.id ? r : response.data))
-        })}
-    const addComment = (review, comment) => {
-        reviewServices.addComment
-        .then(response => {
-            setReviews(reviews.map(r => r.id !== review.id ? r : response.data))
-        })}
-    const editStars = (stars, review) => {
-        reviewServices.editStars
-        .then(response => {
-            setReviews(reviews.map(r => r.id !== review.id ? r : response.data))
-        })}
-    const editContent = (content, review) => {
-        reviewServices.editContent
-        .then(response => {
-            setReviews(reviews.map(r => r.id !== review.id ? r : response.data))
-        })}
-    
-    const baseURL = "/api/"
+	console.log("APP USER: ", user)
+	const addNewUser = (newUser) => {
+		userServices.addNewUser(newUser).then((response) => {
+			console.log(response)
+			setUsers([...users, response.data])
+		})
+	}
+	const followUser = (userToFollow, user) => {
+		userServices.followUser.then((response) => {
+			setUser(response.data)
+			setUsers(users.map((u) => (u.id !== user.id ? u : response.data)))
+		})
+	}
+	const unfollowUser = (userToFollow, user) => {
+		userServices.unfollowUser.then((response) => {
+			setUser(response.data)
+			setUsers(users.map((u) => (u.id !== user.id ? u : response.data)))
+		})
+	}
+	const addWatched = (titleid, user) => {
+		userServices.addWatched.then((response) => {
+			setUser(response.data)
+			setUsers(users.map((u) => (u.id !== user.id ? u : response.data)))
+		})
+	}
+	const addToWatch = (titleid, user) => {
+		userServices.addToWatch.then((response) => {
+			setUser(response.data)
+			setUsers(users.map((u) => (u.id !== user.id ? u : response.data)))
+		})
+	}
+
+	const addNewReview = (newReview) => {
+		reviewServices.addNewReview.then((response) => {
+			console.log(response)
+			setReviews([...reviews, response.data])
+		})
+	}
+	const deleteReview = (review) => {
+		reviewServices.deleteReview.then((response) => {
+			console.log("delete succeeded")
+			const newReviews = reviews.filter((r) => r.id !== review.id)
+			setReviews(newReviews)
+		})
+	}
+	const likeReview = (review, user) => {
+		reviewServices.likeReview.then((response) => {
+			setReviews(
+				reviews.map((r) => (r.id !== review.id ? r : response.data))
+			)
+		})
+	}
+	const unlikeReview = (review, user) => {
+		reviewServices.unlikeReview.then((response) => {
+			setReviews(
+				reviews.map((r) => (r.id !== review.id ? r : response.data))
+			)
+		})
+	}
+	const addComment = (review, comment) => {
+		reviewServices.addComment.then((response) => {
+			setReviews(
+				reviews.map((r) => (r.id !== review.id ? r : response.data))
+			)
+		})
+	}
+	const editStars = (stars, review) => {
+		reviewServices.editStars.then((response) => {
+			setReviews(
+				reviews.map((r) => (r.id !== review.id ? r : response.data))
+			)
+		})
+	}
+	const editContent = (content, review) => {
+		reviewServices.editContent.then((response) => {
+			setReviews(
+				reviews.map((r) => (r.id !== review.id ? r : response.data))
+			)
+		})
+	}
+
+	const baseURL = "/api/"
 
 	useEffect(() => {
 		axios.get(baseURL + "users").then((response) => {
@@ -140,9 +151,14 @@ function App() {
 				<Route path="/register">
 					<Register />
 				</Route>
-				<Route path="/profile">
+				<Route path="/myprofile">
 					<NavBar user={user} setUser={FsetUser} />
-					<Profile />
+					<Profile user={user} />
+				</Route>
+				<Route path="/profile/:username">
+					<NavBar user={user} setUser={FsetUser} />
+					{/* TODO: Fix this so it's not dodgy. If no user input, then check useParams. Had null check issues */}
+					<Profile user="**NO_USER**" />
 				</Route>
 				<Route path="/myMovies">
 					<NavBar />
