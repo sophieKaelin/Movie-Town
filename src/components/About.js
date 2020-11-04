f74d0e162a4fe1bc1d06a5ed89ec80943d2456a4
George Tsamoglou <george.gt39@gmail.com>
2020-11-04 12:31:06 +1100

    changes to about page and carousel

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,35 @@
 import React from "react"
 import PosterCarousel from "./PosterCarousel.js"
+import "bootstrap/dist/css/bootstrap.css"
+import { Jumbotron, Container, Card, Row, Col } from "react-bootstrap"
 
 const About = () => {
-	return <PosterCarousel />
+	return (
+		<div>
+			<Card>
+				<Jumbotron fluid>
+					<Container>
+						<h1>About us</h1>
+						<p>
+							Welcome to our website, made for movie fanatics by
+							movie fanatics. We made this website to connect and
+							share with our friends our passion for movies.
+						</p>
+					</Container>
+				</Jumbotron>
+				<Row>
+					<Col className="d-flex justify-content-center">
+						<h2>Meet our Team</h2>
+					</Col>
+				</Row>
+				<Row>
+					<Col className="d-flex justify-content-center">
+						<PosterCarousel />
+					</Col>
+				</Row>
+			</Card>
+		</div>
+	)
 }
 
 export default About
