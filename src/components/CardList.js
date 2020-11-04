0b58ef6b15f58c90d76914f83f3fc513f4928b94
Sophie Kaelin <sophiekaelin@Sophies-MacBook-Pro.local>
2020-11-04 16:30:39 +1100

    remove review modal from movie list

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -4,7 +4,6 @@ import { Button, Container, Form, Row, Col } from "react-bootstrap"
 import "../style/MovieCard.css"
 
 import MovieCard from "./MovieCard"
-import ReviewModal from "./ReviewModal.js"
 import axios from "axios"
 
 export const CardList = ({
@@ -21,11 +20,6 @@ export const CardList = ({
 }) => {
 	const [movies, setMovies] = useState([])
 	const [watched, setWatched] = useState(true)
-
-	const reviewButtonStyle = {
-		marginTop: "50px",
-		padding: "15px",
-	}
 
 	useEffect(() => {
 		console.log(user)
@@ -57,18 +51,6 @@ export const CardList = ({
 
 	return (
 		<Container>
-			<Row>
-				<Col>
-					<Button
-						className="btn-block"
-						size="lg"
-						style={reviewButtonStyle}
-						onClick={handleShow}
-					>
-						Write Review
-					</Button>
-				</Col>
-			</Row>
 			<Row id="movieListRow">
 				<Col xs={2}></Col>
 				<Col>
@@ -96,14 +78,6 @@ export const CardList = ({
 							/>
 						)
 					})}
-					<ReviewModal
-						user={user}
-						addNewReview={addNewReview}
-						movie={movie}
-						setMovie={setMovie}
-						show={show}
-						handleClose={handleClose}
-					/>
 				</Col>
 				<Col xs={2}></Col>
 			</Row>
