14fb761f03802e84fa0af48867d524ec23df99fd
Sophie Kaelin <sophie.m.kaelin@gmail.com>
2020-11-04 11:24:54 +1100

    reviewModal accessible from cardlist

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,13 +1,27 @@
 import React, { useEffect, useState } from "react"
 import "bootstrap/dist/css/bootstrap.css"
-import { Card, Row, Col, ListGroup, ListGroupItem } from "react-bootstrap"
+import { Button, Container } from "react-bootstrap"
 import "../style/MovieCard.css"
 
 import MovieCard from "./MovieCard"
+import ReviewModal from "./ReviewModal.js"
 import axios from "axios"
 
-export const CardList = ({ user }) => {
+export const CardList = ({
+	user,
+	addNewReview,
+	movie,
+	setMovie,
+	show,
+	handleClose,
+	handleShow,
+}) => {
 	const [movies, setMovies] = useState([])
+
+	const reviewButtonStyle = {
+		marginTop: "50px",
+		padding: "15px",
+	}
 
 	useEffect(() => {
 		console.log(user)
@@ -25,5 +39,30 @@ export const CardList = ({ user }) => {
 		}
 	}, [user])
 
-	return movies.map((m) => <MovieCard movie={m} />)
+	return (
+		<div>
+			<Container>
+				<Button
+					className="btn-block"
+					size="lg"
+					style={reviewButtonStyle}
+					onClick={handleShow}
+				>
+					{" "}
+					Write Review
+				</Button>
+			</Container>
+			{movies.map((m) => (
+				<MovieCard movie={m} />
+			))}
+			<ReviewModal
+				user={user}
+				addNewReview={addNewReview}
+				movie={movie}
+				setMovie={setMovie}
+				show={show}
+				handleClose={handleClose}
+			/>
+		</div>
+	)
 }
