7537d38d5d37d01de0bca8f2bd3dd772f7b4b64d
Cecilia Hollins <hollins.cecilia@gmail.com>
2020-11-05 14:42:26 +1100

    my broken code that is a little less broken

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -11,9 +11,9 @@ const Profile = ({ loggedInUser, followUser, unfollowUser }) => {
 	const userURL = "http://localhost:3001/api/users/"
 	const [user, setUser] = useState({})
 	const [movies, setMovies] = useState([])
+	const [followBtnTxt, setFollowBtnTxt] = useState("")
 
 	let { username } = useParams()
-	console.log("logged in: ", loggedInUser)
 
 	useEffect(() => {
 		axios.get(userURL + username).then((response) => {
@@ -30,12 +30,19 @@ const Profile = ({ loggedInUser, followUser, unfollowUser }) => {
 		})
 	}, [])
 
-	const followButtonFn = () => {
-		followUser(username, loggedInUser)
-	}
+	//THIS IS BROKEN AND GUESS THE FUCK WHAT, I CANT FIGURE OUT WHY IF MY FUCKING LIFE DEPENDEDED ON IT
+	// if (loggedInUser !== undefined) {
+	// 	console.log(loggedInUser === null)
+	// 	console.log(loggedInUser)
+	// 	if (loggedInUser.follows.includes(username)) {
+	// 		setFollowBtnTxt("Unfollow")
+	// 	} else {
+	// 		setFollowBtnTxt("Follow")
+	// 	}
+	// }
 
-	const unfollowButtonFn = () => {
-		unfollowUser(username, loggedInUser)
+	const followButtonFn = () => {
+		followBtnTxt === "Follow" ? followUser(username, loggedInUser) : unfollowUser(username, loggedInUser)
 	}
 
 	return (
@@ -47,8 +54,9 @@ const Profile = ({ loggedInUser, followUser, unfollowUser }) => {
 					className="profileImage"
 				/>
 				<h3>{user.username}</h3>
-				{/* Munted code that should work and doesnt work I will get it working eventually */}
-				{/* {user.username === loggedInUser.username
+				{/* WHAT THE HONEST TO GOD FUCKERY IS THIS SOMEONE FIX THIS FOR ME GOD FUCKING DAMN */}
+	
+				{user.username === loggedInUser.username
 					? ( <Button variant="primary" className="editBtn" size="sm">
 							Edit
 							<svg
@@ -64,56 +72,12 @@ const Profile = ({ loggedInUser, followUser, unfollowUser }) => {
 								/>
 							</svg>
 						</Button>
-					) : (loggedInUser.follows.includes(username))
-					? ( <Button
-							variant="primary"
-							className="followBtn"
-							onClick={unfollowButtonFn}>
-							Unfollow
-						</Button>)
-					: (<Button
+					) : (<Button
 							variant="primary"
 							className="followBtn"
 							onClick={followButtonFn}>
-							Follow
-						</Button>
-					)
-					} */}
-
-				{/* {user.username === loggedInUser.username ? (
-					<Button variant="primary" className="editBtn" size="sm">
-						Edit
-						<svg
-							width="1em"
-							height="1em"
-							viewBox="0 0 16 16"
-							class="bi bi-pen-fill ml-2 mb-1"
-							fill="currentColor"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								fill-rule="evenodd"
-								d="M13.498.795l.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001z"
-							/>
-						</svg>
-					</Button>
-				) : loggedInUser.follows.includes(username) ? (
-					<Button
-						variant="primary"
-						className="followBtn"
-						onClick={unfollowButtonFn}
-					>
-						Unfollow
-					</Button>
-				) : (
-					<Button
-						variant="primary"
-						className="followBtn"
-						onClick={followButtonFn}
-					>
-						Follow
-					</Button>
-				)} */}
+							{followBtnTxt}
+						</Button>)}
 			</Jumbotron>
 			<Container>
 				<Row>
