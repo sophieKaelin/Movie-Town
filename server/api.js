b4109089a944b710fbfd1293318bbd981fcea756
Cecilia Hollins <hollins.cecilia@gmail.com>
2020-11-08 20:14:27 +1100

    fixing the comments

--- server/api.js
+++ server/api.js
@@ -226,6 +226,7 @@ apiRouter.put("/api/reviews/:id/comments", (req, res, next) => {
 		comments: body.comments,
 	}
 
+	console.log(review)
 	Review.findByIdAndUpdate(req.params.id, review, { new: true })
 		.then((updatedReview) => {
 			res.json(updatedReview)
